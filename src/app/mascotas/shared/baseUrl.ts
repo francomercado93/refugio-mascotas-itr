d581f8f6b49384f728227f5b6cada6131671ab1b
francomercado93 <mercadofranco.a@gmail.com>
2020-01-07 15:48:31 -0300

    Auto-generated commit

--- src/app/mascotas/shared/baseUrl.ts
+++
@@ -1,5 +0,0 @@
-export const baseUrl = 'http://localhost:3000/api'
-
-export const baseUrlApiLocal = 'http://localhost:8090/'
-
-export const baseUrlApiHeroku = 'https://refugio-java.herokuapp.com/api'
--- src/app/mascotas/shared/tipoAnimal.ts
+++
@@ -1,6 +0,0 @@
-export interface TipoAnimal {
-
-    id: String
-    nombre: string
-
-}
